023a8a22e1bd18ccdd32ea93296b0d0253dc5f2c
Ridley Larsen <larsenridley@gmail.com>
2017-02-02 17:01:43 -0700

    Added more plays.

--- python/monologue.py
+++ python/monologue.py
@@ -3,15 +3,28 @@ import requests
 import random
 from bs4 import BeautifulSoup
 
+PLAY_URIS = [
+    "http://shakespeare.mit.edu/macbeth/full.html",
+    "http://shakespeare.mit.edu/hamlet/full.html",
+    "http://shakespeare.mit.edu/allswell/full.html",
+    "http://shakespeare.mit.edu/measure/full.html",
+    "http://shakespeare.mit.edu/merchant/full.html",
+    "http://shakespeare.mit.edu/merry_wives/full.html",
+    "http://shakespeare.mit.edu/much_ado/full.html",
+    "http://shakespeare.mit.edu/taming_shrew/full.html",
+    "http://shakespeare.mit.edu/twelfth_night/full.html"
+]
+
 def main():
-    response = requests.get("http://shakespeare.mit.edu/macbeth/full.html")
+    uri = random.choice(PLAY_URIS)
+    response = requests.get(uri)
     content = response.content.replace("\n", "")
     soup = BeautifulSoup(content, "html.parser")
     all_quotes = soup.find_all('a', {"name": re.compile("speech[0-9]{1,}")})
     monologues = []
     for name in all_quotes:
         quote = name.next_sibling
-        if (len(quote.findChildren()) > 4):
+        if (len(quote.findChildren()) > 8):
             text = name.text.upper() + "\n"
             for line in quote.findChildren():
                 if line.name == "a":
